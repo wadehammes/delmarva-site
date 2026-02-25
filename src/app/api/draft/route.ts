998f74fc17afe9d52ac27ba72507926047001a19
Wade Hammes <wade.hammes@gmail.com>
2026-02-24 19:09:44 -0500

    Fix Draft Mode

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -1,17 +1,22 @@
 import { draftMode } from "next/headers";
-import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
   const draft = await draftMode();
   const { searchParams } = new URL(request.url);
 
-  if (
-    searchParams.get("previewSecret") !== process.env.CONTENTFUL_PREVIEW_SECRET
-  ) {
+  const secret = searchParams.get("secret");
+
+  if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
     return new Response("Invalid token", { status: 401 });
   }
 
+  const redirectPath = searchParams.get("path") ?? "";
+  const path = `/${redirectPath}`.replace(/\/+/g, "/");
+
   draft.enable();
 
-  redirect(`/${searchParams.get("redirect") || ""}`);
+  const redirectUrl = new URL(path, request.url);
+
+  return NextResponse.redirect(redirectUrl, 307);
 }
